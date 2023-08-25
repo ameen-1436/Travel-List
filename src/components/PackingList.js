51d897412d45aed5ca1142883a86f51db63b0abb
ameen-1436 <50878641+ameen-1436@users.noreply.github.com>
2023-08-25 12:45:49 +0530

    Update PackingList.js

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,27 +1,24 @@
-import { useState } from "react";
-import Item from "./Item";
-
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
+import {useState} from 'react';
 export default function PackingList({
   items,
-  onDeleteItem,
+  onRemoveItem,
   onToggleItem,
-  onClearList,
+  listCountState,
+  removeCount,
+  onClearList
 }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
+  if(sortBy === "input") sortedItems = items;
 
-  if (sortBy === "input") sortedItems = items;
+  if(sortBy === "description") sortedItems = items.slice().sort((a,b)=> a.description.localeCompare(b.description))
 
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  if(sortBy === "packed") sortedItems = items.slice().sort((a,b)=>Number(b.packed) - Number(a.packed) )
 
   return (
     <div className="list">
@@ -29,21 +26,42 @@ export default function PackingList({
         {sortedItems.map((item) => (
           <Item
             item={item}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
             key={item.id}
+            onRemoveItem={onRemoveItem}
+            onToggleItem={onToggleItem}
+            listCountState={listCountState}
+            removeCount={removeCount}
           />
         ))}
       </ul>
-
       <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <select value={sortBy} onChange={e=>setSortBy(e.target.value)} >
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
+          <option value="packed">Sort by packed Status</option>
         </select>
-        <button onClick={onClearList}>Clear list</button>
+        <button onClick={()=>onClearList()}>Clear List</button>
       </div>
     </div>
+  );
+}
+function Item({ item, key, onRemoveItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
+      <span
+        style={item.packed ? { textDecoration: "line-through" } : {}}
+        key={item.id}
+      >
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onRemoveItem(item.id)}>❌</button>
+    </li>
   );
 }
