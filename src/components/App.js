a98977fba3e32c4b0e1274d092d3a0dacbb76648
ameen-1436 <50878641+ameen-1436@users.noreply.github.com>
2023-08-25 12:44:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,45 +3,47 @@ import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
+import Accordian from './accordian';
 
 export default function App() {
   const [items, setItems] = useState([]);
-
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]);
-  }
-
-  function handleDeleteItem(id) {
+  // const [addCount, setAddCount] = useState(0);
+  // const [packCount, setPackCount] = useState(0);
+  const handleAddItems = (item) => {
+    return setItems((items) => [...items, item]);
+  };
+  const handleRemoveItems = (id) => {
     setItems((items) => items.filter((item) => item.id !== id));
-  }
+  };
 
-  function handleToggleItem(id) {
+  const handleToggleItem = (id) => {
     setItems((items) =>
       items.map((item) =>
         item.id === id ? { ...item, packed: !item.packed } : item
       )
     );
-  }
+  };
 
-  function handleClearList() {
+  const handleClearItemsList = () => {
     const confirmed = window.confirm(
-      "Are you sure you want to delete all items?"
+      "Are you sure you want to clear the list of items"
     );
 
     if (confirmed) setItems([]);
-  }
+  };
 
   return (
-    <div className="app">
-      <Logo />
+    <div>
+      {/* <Logo />
       <Form onAddItems={handleAddItems} />
       <PackingList
         items={items}
-        onDeleteItem={handleDeleteItem}
+        onRemoveItem={handleRemoveItems}
         onToggleItem={handleToggleItem}
-        onClearList={handleClearList}
+        onClearList={handleClearItemsList}
       />
-      <Stats items={items} />
+      <Stats items={items} /> */}
+      <Accordian />
     </div>
   );
 }
